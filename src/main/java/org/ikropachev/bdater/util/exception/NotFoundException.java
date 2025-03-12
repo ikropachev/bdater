83f9d981a9c572d455d529e7216d95a0622a224d
Rock133 <paporotnik796@gmail.com>
2025-03-12 22:41:49 +0300

    Abstract test classes

---
+++ src/main/java/org/ikropachev/bdater/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.ikropachev.bdater.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
